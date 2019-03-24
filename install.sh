d295034d45b0464c537f2973c88d6e836007114b
Daniel Brandenburg <jdb12@rice.edu>
2019-03-24 08:35:11 -0400

    Install target now in SCONs, no longer a sep shell script

--- install.sh
+++
@@ -1,7 +0,0 @@
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-# Headers
-mkdir -p /usr/local/lib
-ln -sf ${DIR}/libRootAna.a /usr/local/lib/libRootAna.a
-
-mkdir -p /usr/local/include/RootAna
-cp *.h /usr/local/include/RootAna/
